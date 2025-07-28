85228ed86e0c7cdbc1feb5aa8d0d5637dff198c4
Leonardo Bigeschi <leobigeschi21@gmail.com>
2025-07-28 17:33:25 -0300

    Atualizando carrossel de marcas mobile e tablets.

--- src/components/Carousel/CarouselBrands/CarouselBrandsContent/index.tsx
+++ src/components/Carousel/CarouselBrands/CarouselBrandsContent/index.tsx
@@ -5,44 +5,37 @@ import { Card, CardContent } from "@/components/ui/card"
 import { CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 import { logos } from "@/constants/logos"
 import { useIsMobile } from "@/hooks/useIsMobile"
-import { CarouselBrandsMobileTablets } from "../CarouselBrandsMobileTablets"
 
 export const CarouselBrandsContent = () => {
     const isMobile = useIsMobile();
 
     return isMobile ? (
-        // <>
-        //     <CarouselContent className="flex gap-2 px-2 touch-pan-x">
-        //         {logos.map((item) => (
-        //             <div>
-        //                 <CarouselItem
-        //                 key={item.id}
-        //                 className="px-2"
-        //             >
-        //                 <Card className="shadow-none border-none bg-transparent">
-        //                     <CardContent className="p-0 flex justify-center">
-        //                         <div className="relative w-[100px] h-[100px] rounded-full overflow-hidden border border-zinc-200">
-        //                             <Image
-        //                                 src={item.image}
-        //                                 alt={item.name}
-        //                                 fill
-        //                                 className="object-cover cursor-pointer"
-        //                             />
-        //                         </div>
-        //                     </CardContent>
-        //                 </Card>
-        //             </CarouselItem>
-        //             </div>
-        //         ))}
-        //     </CarouselContent>
-
-        //     {/* <CarouselPrevious className="flex absolute left-2 top-1/2 -translate-y-1/2 z-10" />
-        //     <CarouselNext className="flex absolute right-2 top-1/2 -translate-y-1/2 z-10" /> */}
-
-        // </>
-
         <>
-            <CarouselBrandsMobileTablets />
+            <CarouselContent className="flex gap-2 px-2 touch-pan-x">
+                {logos.map((item) => (
+                    <CarouselItem
+                        key={item.id}
+                        className="px-2"
+                    >
+                        <Card className="shadow-none border-none bg-transparent">
+                            <CardContent className="p-0 flex justify-center">
+                                <div className="relative w-[100px] h-[100px] rounded-full overflow-hidden border border-zinc-200">
+                                    <Image
+                                        src={item.image}
+                                        alt={item.name}
+                                        fill
+                                        className="object-cover cursor-pointer"
+                                    />
+                                </div>
+                            </CardContent>
+                        </Card>
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+
+            {/* <CarouselPrevious className="flex absolute left-2 top-1/2 -translate-y-1/2 z-10" />
+            <CarouselNext className="flex absolute right-2 top-1/2 -translate-y-1/2 z-10" /> */}
+
         </>
     )
         : (
--- src/components/Sections/SectionCarousel/SectionCarouselBrands/index.tsx
+++ src/components/Sections/SectionCarousel/SectionCarouselBrands/index.tsx
@@ -4,7 +4,7 @@ export const SectionCarouselBrands = () => {
     return (
         <section className="w-full px-4 lg:px-0 mt-20">
             <div className="flex flex-col items-center lg:justify-center gap-6">
-                <h1 className="text-base md:text-3xl font-bold text-center lg:text-left whitespace-nowrap">
+                <h1 className="text-2xl md:text-3xl font-bold text-center lg:text-left whitespace-nowrap">
                     Algumas de nossas marcas
                 </h1>
 
